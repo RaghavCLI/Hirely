b26b2f455ed820ed1496e3a10d3b84bb8d08bc87
viper-raghav <upadhyayraghav17@gmail.com>
2025-06-01 17:32:25 +0530

    supabase db setup & job fetch api

--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import { useSession } from "@clerk/clerk-react";
+import { getJobs } from "../../src/api/apijobs";
+import { useEffect } from "react";
 
 function JobListing() {
+  const { session } = useSession();
+
+  const fetchJobs = async () => {
+    const supabaseAccessToken = await session.getToken({
+      template: "supabase",
+    });
+    const data = await getJobs(supabaseAccessToken);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
   return (
     <div>
       <h1>Job Listing Page</h1>
